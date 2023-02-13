cfc4e0010e7715c52d714761b9e9c277605a0dbc
Blui322 <luistejada@pursuit.com>
2023-02-13 13:17:46 -0500

    lets get it one step at the time

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const { TestWatcher } = require("jest");
+
 /**
  * Returns all of the "categories" from the assignments object.
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  /**  created a new obj variable, gave the value of a built in method .key and return that new variable */
+  let obj = Object.keys(assignments);
+  return obj
+}
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +18,12 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score,) {
+  /** created 2 new variables and used the built in method Math.round to round a result to the nearest integer */
+  let score1 = score.received 
+  let score2 = score.max
+  return Math.round(score1/ score2 * 100);
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +33,10 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  /** use the same formula for the second fuction with the extra built in method math.max to takes in any amount of numbers and returns the highest one */
+  return Math.max(Math.round(score1.received / score1.max * 100), Math.round(score2.received / score2.max * 100));
+}
 
 // Don't change anything below this line.
 module.exports = {
