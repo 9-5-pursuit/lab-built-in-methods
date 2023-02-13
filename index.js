6ec31743d2a1ffb59c8338e1d401d9321bb84922
Jennifer <jenniferpeterson@pursuit.org>
2023-02-13 12:41:59 -0500

    function three complete

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ function getPercentageScore(score) {
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  let percentage1 = (score1.received / score1.max) * 100;
+  let percentage2 = (score2.received / score2.max) * 100;
+
+  return Math.round(Math.max(percentage1, percentage2));
+}
 
 // Don't change anything below this line.
 module.exports = {
