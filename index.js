45ed412f2c57f663bfca2d265ce5a00c57afff6e
Erika Medina <erikamedina@Erikas-MacBook-Air.local>
2023-02-14 06:34:00 -0500

    builtInMethods

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  * @param {object} assignments - An object with assignments organized by category.
  * @returns {string[]} An array of strings, which represent all of the keys in the object.
  */
-function getAssignmentTypes(assignments) {}
+function getAssignmentTypes(assignments) {
+  // object = assignments keys = all the keys and pair values
+return Object.keys(assignments)
+}
+
 
 /**
  * Returns an integer that represents the score as a percentage.
@@ -12,7 +16,14 @@ function getAssignmentTypes(assignments) {}
  * @param {number} score.max - The maximum amount of points that could be received on the assignment.
  * @returns {number} A rounded integer, representing a percentage.
  */
-function getPercentageScore(score) {}
+function getPercentageScore(score) {
+
+  let percentage = (score.received / score.max) * 100
+ 
+  // get percentage 14/18 = 0.77778 then multiply by 100 to get percentage
+ //we need rounded in math.round
+  return Math.round(percentage)
+}
 
 /**
  * Given two score objects, returns an integer that represents the highest percentage received.
@@ -22,7 +33,19 @@ function getPercentageScore(score) {}
  * @param {object} score2 - An object representing another score of an assignment. Has the same shape as `score1`.
  * @returns {number} A rounded integer, representing the highest percentage scored among the two given scores.
  */
-function getHighestOfTwoScores(score1, score2) {}
+function getHighestOfTwoScores(score1, score2) {
+  //get first score and second score
+  // turn them into percentages
+  // compare and get max score of the two
+
+let firstScore = (score1.received / score1.max) * 100
+let secondScore = (score2.received / score2.max) * 100
+
+return Math.round(Math.max(firstScore, secondScore))
+
+
+
+}
 
 // Don't change anything below this line.
 module.exports = {
